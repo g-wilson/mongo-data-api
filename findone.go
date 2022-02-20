12367c361b8b4bbde8357515f637d60528d2f5be
George Wilson <george@g-wilson.co.uk>
2022-02-20 22:38:44 +0000

    change fineOne action to use options same as find

--- findone.go
+++ findone.go
@@ -8,6 +8,26 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+type FindOneOptions struct {
+	underlying map[string]interface{}
+}
+
+func NewFindOneOptions() FindOneOptions {
+	return FindOneOptions{underlying: map[string]interface{}{}}
+}
+
+func (o FindOneOptions) WithProjection(proj bson.M) FindOneOptions {
+	o.underlying["projection"] = proj
+
+	return o
+}
+
+func (o FindOneOptions) Apply(params bson.M) {
+	for k, v := range o.underlying {
+		params[k] = v
+	}
+}
+
 type FindOneResponse struct {
 	Error    error
 	Document json.RawMessage
@@ -26,31 +46,14 @@ func (r FindOneResponse) Decode(dest interface{}) error {
 	return nil
 }
 
-func (c *Collection) FindOne(ctx context.Context, filter bson.M) FindOneResponse {
+func (c *Collection) FindOne(ctx context.Context, filter bson.M, options ...FindOneOptions) FindOneResponse {
 	body := bson.M{
 		"collection": c.name,
 		"filter":     filter,
 	}
 
-	res := struct {
-		Document json.RawMessage
-	}{}
-	err := c.database.do(ctx, ActionFindOne, body, &res)
-	if err != nil {
-		return FindOneResponse{Error: err}
-	}
-	if res.Document == nil {
-		return FindOneResponse{Error: ErrNoDocuments}
-	}
-
-	return FindOneResponse{Document: res.Document}
-}
-
-func (c *Collection) FindOneWithProjection(ctx context.Context, filter, projection bson.M) FindOneResponse {
-	body := bson.M{
-		"collection": c.name,
-		"filter":     filter,
-		"projection": projection,
+	for _, o := range options {
+		o.Apply(body)
 	}
 
 	res := struct {
